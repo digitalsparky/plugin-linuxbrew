e5ea0881575957894a405f0f632f04573a2af0ba
Matt Spurrier <matthew@spurrier.com.au>
2018-10-31 03:06:41 +0800

    Update config
    Config updates for latest working version

--- init.fish
+++ init.fish
@@ -1,17 +1,18 @@
-set -l linuxbrew_bin_path "$HOME/.linuxbrew/bin"
-set -l linuxbrew_sbin_path "$HOME/.linuxbrew/sbin"
-set -l linuxbrew_manpath "$HOME/.linuxbrew/share/man"
-set -l linuxbrew_infopath "$HOME/.linuxbrew/share/info"
+set -g __linuxbrew_dir /home/linuxbrew/.linuxbrew
 
-contains -- $linuxbrew_bin_path $PATH
-  or set -gx PATH $linuxbrew_bin_path $PATH
+if test -d $__linuxbrew_dir
+  if not contains -- $__linuxbrew_dir/bin $PATH
+    set PATH $__linuxbrew_dir/bin $PATH
+  end
+  if not contains -- $__linuxbrew_dir/sbin $PATH
+    set PATH $__linuxbrew_dir/sbin $PATH
+  end
+  if not contains -- $__linuxbrew_dir/share/man $MANPATH
+    set MANPATH $__linuxbrew_dir/share/man $MANPATH
+  end
+  if not contains -- $__linuxbrew_dir/share/info $INFOPATH
+    set INFOPATH $__linuxbrew_dir/share/info $INFOPATH
+  end
+end
 
-contains -- $linuxbrew_sbin_path $PATH
-  or set -gx PATH $linuxbrew_sbin_path $PATH
-
-contains -- $linuxbrew_manpath $MANPATH
-  or set -gx MANPATH $linuxbrew_manpath $MANPATH
-
-contains -- $linuxbrew_infopath $INFOPATH
-  or set -gx INFOPATH $linuxbrew_infopath $INFOPATH
-
+set -e __linuxbrew_dir
